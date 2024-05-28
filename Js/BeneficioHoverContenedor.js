196cf90f87af82d53ff38360768f15d0fd2ef406
Alecit03 <u20221c488@upc.edu.pe>
2024-05-28 14:43:46 -0500

    feat:Añadi transiciones para los testimonios

--- Js/BeneficioHoverContenedor.js
+++ Js/BeneficioHoverContenedor.js
@@ -1,13 +1,13 @@
-function mostrartexto(elemento, textoAdicional) {
-    var textoAdicionalElement = elemento.querySelector('.texto-adicional');
+function Beneficio_Mostrartexto(elemento, textoAdicional) {
+    var textoAdicionalElement = elemento.querySelector('.Beneficio-texto-adicional');
     textoAdicionalElement.innerHTML = textoAdicional;
     textoAdicionalElement.style.display = 'block';
     elemento.classList.add('expanded');
 }
 
-function ocultartexto(elemento) {
+function Beneficio_Ocultartexto(elemento) {
     elemento.classList.remove('expanded');
-    var textoAdicionalElement = elemento.querySelector('.texto-adicional');
+    var textoAdicionalElement = elemento.querySelector('.Beneficio-texto-adicional');
     textoAdicionalElement.style.display = 'none'; // Hide text when not hovered
     textoAdicionalElement.innerHTML = ''; // Clear text to avoid lingering content
 }
